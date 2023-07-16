65677f769cf297f27987c81bcf6487c0e432665a
Anderson Álvarez <al.alvarez439@gmail.com>
2023-07-16 17:37:00 -0500

    [serial]: Implementation of a temporary parseArguments function [2]

--- src/serial/serial.cpp
+++ src/serial/serial.cpp
@@ -7,7 +7,7 @@
 
 //Set the matrices dimensions, initially 2000
 #define DIM 100
-unsigned int NUMTIMES = 10;
+
 
 //Set the matrices, matrixA and matrixB will be the input matrices
 //matrixC will be the multiply result
@@ -16,23 +16,13 @@ double matrixB[DIM][DIM];
 double matrixC[DIM][DIM];
 
 void fillMatrices (int n);
-double multiplyMatrices (int n);
-//void parseArguments(int argc, char *argv[]);
+
+double multiplyMatrices (int x);
 
 int main(int argc, char *argv[])
 {
-    std::cout
-        << "MMBench (Matrix Multiplication Benchmark)" << std::endl
-        << "Test Version" << std::endl;
-    
-    std::cout 
-        << "Matrices dimensions: " << DIM << std::endl;
-
-    int n = DIM;
-
-    //parseArguments(argc, argv);
-    fillMatrices(n);
-    double time = multiplyMatrices(n);
+    fillMatrices(DIM);
+    double time = multiplyMatrices(DIM);
 
     std::cout << "Time elapsed for multiply the matrices was: " << time << " seconds" << std::endl;
 
@@ -53,7 +43,7 @@ void fillMatrices (int n)
     }
 }
 
-double  multiplyMatrices (int n)
+double  multiplyMatrices (int x)
 {
     int i, j, k;
 
@@ -61,12 +51,12 @@ double  multiplyMatrices (int n)
 
     auto t1 = std::chrono::high_resolution_clock::now();
 
-    for (i = 0; i < n; i++)
+    for (i = 0; i < x; i++)
     {
-        for (j = 0; j < n; j++)
+        for (j = 0; j < x; j++)
         {
             matrixC[i][j] = 0;
-            for (k = 0; k < n; k++)
+            for (k = 0; k < x; k++)
             {
                 matrixC[i][j] = matrixC[i][j] + matrixA[i][k] * matrixB[k][j];
             }
@@ -79,64 +69,3 @@ double  multiplyMatrices (int n)
 
     return totalTime;
 }
-
-
-/*
-int parseUInt(const char *str, unsigned int *output)
-{
-  char *next;
-  *output = strtoul(str, &next, 10);
-  return !strlen(next);
-}
-
-int parseInt(const char *str, int *output)
-{
-  char *next;
-  *output = strtol(str, &next, 10);
-  return !strlen(next);
-}
-
-void parseArguments(int argc, char *argv[])
-{
-    for (int i = 1; i < argc; i++)
-    {
-        if (!std::string("--dimsize").compare(argv[i]))
-        {
-            if (++i >= argc || DIM < 0)
-            {
-                std::cerr << "Invalid dimensions size" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (!std::string("--ntimes").compare(argv[i]))
-        {
-            if (++i >= argc || !parseUInt(argv[i], &NUMTIMES))
-            {
-                std::cerr << "Invalid number of times" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-            if (NUMTIMES < 1)
-            {
-                std::cerr << "Number of times must be  1 or more" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (!std::string("--help").compare(argv[i]))
-        {
-            std::cout << std::endl;
-            std::cout << "Usage of MMBench (Matrix Multiplication Benchmark)" << std::endl;
-            std::cout << "Options:" << std::endl;
-            std::cout << "   --help                  Print this message" << std::endl;
-            std::cout << "   --dimsize    DIM        Use DIM for matrices" << std::endl;
-            std::cout << "   --ntimes     NUMTIMES   Run the MMBench NUMTIMES" << std::endl;
-            std::cout << std::endl;
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            std::cerr << "Unrecognized argument" << argv[i] << "(try '--help')" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-    }  
-}
-*/
