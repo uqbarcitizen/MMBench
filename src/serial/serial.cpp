5f590b3f130ca9d6f5306c6a10d119995aa2d289
Anderson Álvarez <al.alvarez439@gmail.com>
2023-07-17 20:12:18 -0500

    [serial]: Implementation of a temporary parseArguments function [3]

--- src/serial/serial.cpp
+++ src/serial/serial.cpp
@@ -5,67 +5,86 @@
 #include <chrono>
 #include <cstring>
 
-//Set the matrices dimensions, initially 2000
-#define DIM 100
-
-
-//Set the matrices, matrixA and matrixB will be the input matrices
-//matrixC will be the multiply result
-double matrixA[DIM][DIM];
-double matrixB[DIM][DIM];
-double matrixC[DIM][DIM];
-
-void fillMatrices (int n);
-
-double multiplyMatrices (int x);
+double matrixMultiply(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-    fillMatrices(DIM);
-    double time = multiplyMatrices(DIM);
+   //Set the matrix dimensions (dim x dim)
+   int dim = 10;
 
-    std::cout << "Time elapsed for multiply the matrices was: " << time << " seconds" << std::endl;
+   //Read command line arguments
+   for (int i = 0; i < argc; i++)
+   {
+        if ((strcmp(argv[i], "-D") == 0 ) || (strcmp(argv[i], "--dimensions") == 0))
+        {
+            dim = atoi(argv[++i]);
+        }
+        else if ((strcmp(argv[i], "-H") == 0) || (strcmp(argv[i], "--help") == 0))
+        {
+            std::cout << std::endl;
+            std::cout << "Options:" << std::endl;
+            std::cout << "--dimensions (-D) [int]   Set the dimensions of matrix (dim x dim)" 
+                      << std::endl;
+            std::cout << "--help (-H)               Display this message"
+                      << std::endl;
+            std::cout << std::endl;
+            exit(1);
+        } 
+   } 
 
-    return 0;
+   double x = matrixMultiply(argc, argv);
+
+   //std::cout << x << std::endl;
+
+   return 0;
 }
 
-void fillMatrices (int n)
+double matrixMultiply(int argc, char *argv[])
 {
-    srand(time(NULL));
+    //Store dimensions argument
+    int n = atoi(argv[2]);
 
+    //Define matrices statically
+    int A[n][n], B[n][n], C[n][n];
+
+    //Matrix A init
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            matrixA[i][j] = rand();
-            matrixB[i][j] = rand();
+            A[i][j] = 1;
         }
     }
-}
 
-double  multiplyMatrices (int x)
-{
-    int i, j, k;
-
-    double totalTime;
-
-    auto t1 = std::chrono::high_resolution_clock::now();
-
-    for (i = 0; i < x; i++)
+    //Matrix B init
+    for (int i = 0; i < n; i++)
     {
-        for (j = 0; j < x; j++)
+        for (int j = 0; j < n; j++)
         {
-            matrixC[i][j] = 0;
-            for (k = 0; k < x; k++)
+            B[i][j] = 1;
+        }
+    }
+
+    //Matrix C init
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            C[i][j] = 0;
+        }
+    }
+
+    //Perform the matrix multiplication -> C = A*B
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
             {
-                matrixC[i][j] = matrixC[i][j] + matrixA[i][k] * matrixB[k][j];
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 
-    auto t2 = std::chrono::high_resolution_clock::now();
-
-    totalTime = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
-
-    return totalTime;
+    return 0;
 }
